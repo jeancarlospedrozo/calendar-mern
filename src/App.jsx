a6cd8882e128df9daeca87732e4ac72afae142a2
Jean Carlos <jeancapedrozo@gmail.com>
2023-04-14 00:38:43 -0500

    configurar rutas iniciales

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Login from "./auth/pages/Login";
+import Calendar from "./calendar/pages/Calendar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const authStatus = "not-authenticated";
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Routes>
+      {authStatus === "not-authenticated" ? (
+        <Route path="auth/*" element={<Login />} />
+      ) : (
+        <Route path="*" element={<Calendar />} />
+      )}
+      <Route path="*" element={<Navigate to={"auth/login"} />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
