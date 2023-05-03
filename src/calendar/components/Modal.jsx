a1f9e54e5c0846d531b8dc54fce2b28fd002e0d7
Jean Carlos <jeancapedrozo@gmail.com>
2023-05-03 00:58:33 -0500

    Setup datepicker and formik

--- src/calendar/components/Modal.jsx
+++ src/calendar/components/Modal.jsx
@@ -1,15 +1,27 @@
+import { addHours } from "date-fns";
+import es from "date-fns/locale/es";
+import { Field, Form, Formik, useFormikContext } from "formik";
 import React, { useState } from "react";
-import DatePicker from "react-datepicker";
+import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 export const Modal = ({ isVisible, onClose }) => {
   const [startDate, setStartDate] = useState(new Date());
   if (!isVisible) return null;
 
+  registerLocale("es", es);
+
+  const formValues = {
+    title: "",
+    notes: "",
+    startDate: new Date(),
+    endDate: addHours(new Date(), 2),
+  };
+
   return (
     <div
       id="defaultModal"
-      tabindex="-1"
+      tabIndex="-1"
       aria-hidden="true"
       className="fixed inset-0 z-20 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm"
     >
@@ -17,102 +29,96 @@ export const Modal = ({ isVisible, onClose }) => {
         <div className="relative rounded-lg bg-white shadow dark:bg-gray-700">
           <button
             type="button"
-            class="absolute right-2.5 top-3 ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-white"
+            className="absolute right-2.5 top-3 ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-white"
             data-modal-hide="authentication-modal"
             onClick={onClose}
           >
             <svg
               aria-hidden="true"
-              class="h-5 w-5"
+              className="h-5 w-5"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               ></path>
             </svg>
-            <span class="sr-only">Close modal</span>
+            <span className="sr-only">Close modal</span>
           </button>
-          <div class="px-6 py-6 lg:px-8">
-            <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">
+          <div className="px-6 py-6 lg:px-8">
+            <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
               Nuevo evento
             </h3>
-            <form class="space-y-6" action="#">
-              <div>
-                <label class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
-                  Fecha y hora de inicio
-                </label>
-                <DatePicker
-                  name="startDate"
-                  selected={startDate}
-                  dateFormat="Pp"
-                  onChange={(date) => setStartDate(date)}
-                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
-                />
-                {/* <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
-                  placeholder="name@company.com"
-                  required
-                /> */}
-              </div>
-              <div>
-                <label class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
-                  Fecha y hora fin
-                </label>
-                <DatePicker
-                  name="endDate"
-                  selected={startDate}
-                  dateFormat="Pp"
-                  onChange={(date) => setStartDate(date)}
-                  className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
-                />
-
-                {/* <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  placeholder="••••••••"
-                  class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
-                  required
-                /> */}
-              </div>
-              <div>
-                <label class="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
-                  Título y notas
-                </label>
-                <input
-                  type="text"
-                  name="password"
-                  class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
-                  required
-                />
-              </div>
-              <div>
-                <label
-                  for="password"
-                  class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
-                >
-                  Descripción del evento
-                </label>
-                <textarea
-                  name="notes"
-                  id="notes"
-                  class="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
-                ></textarea>
-              </div>
-              <button
-                type="submit"
-                class="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Guardar
-              </button>
-            </form>
+            <Formik
+              initialValues={formValues}
+              onSubmit={(values) => console.log(values)}
+            >
+              {({ values, setFieldValue }) => (
+                <Form className="space-y-6">
+                  <div>
+                    <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+                      Fecha y hora de inicio
+                    </label>
+                    <DatePicker
+                      name="startDate"
+                      selected={values.startDate}
+                      dateFormat="Pp"
+                      showTimeSelect
+                      locale="es"
+                      timeCaption="Hora"
+                      onChange={(date) => setFieldValue("startDate", date)}
+                      className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
+                    />
+                  </div>
+                  <div>
+                    <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+                      Fecha y hora fin
+                    </label>
+                    <DatePicker
+                      name="endDate"
+                      selected={values.endDate}
+                      minDate={values.startDate}
+                      dateFormat="Pp"
+                      showTimeSelect
+                      locale="es"
+                      timeCaption="Hora"
+                      onChange={(date) => setFieldValue("endDate", date)}
+                      className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
+                    />
+                  </div>
+                  <div>
+                    <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+                      Título y notas
+                    </label>
+                    <Field
+                      type="text"
+                      name="title"
+                      className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
+                      required
+                    />
+                  </div>
+                  <div>
+                    <label className="mb-2 block text-sm font-medium text-gray-900 dark:text-white">
+                      Descripción del evento
+                    </label>
+                    <Field
+                      as="textarea"
+                      name="notes"
+                      className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-500 dark:bg-gray-600 dark:text-white dark:placeholder-gray-400"
+                    ></Field>
+                  </div>
+                  <button
+                    type="submit"
+                    className="rounded-lg bg-blue-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    Guardar
+                  </button>
+                </Form>
+              )}
+            </Formik>
           </div>
         </div>
       </div>
